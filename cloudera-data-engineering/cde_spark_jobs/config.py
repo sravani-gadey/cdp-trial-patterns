2703bb6d968f96f4c0f45c818df60130ae7febd0
sravani-gadey <110522171+sravani-gadey@users.noreply.github.com>
2025-01-16 16:16:38 +0530

    Update config.py

--- cloudera-data-engineering/cde_spark_jobs/config.py
+++ cloudera-data-engineering/cde_spark_jobs/config.py
@@ -1,2 +1,2 @@
-storageLocation = "s3a://bptipl35rwks/trial-cde-data" #copy and paste values here from Trial Manager configuration
-username = "csso_sravani.gadey" #copy and paste values here from Trial Manager configuration
+storageLocation = "s3a://owspll9mdt3w/trial-cde-data" #copy and paste values here from Trial Manager configuration
+username = "csso_trialtesting" #copy and paste values here from Trial Manager configuration
